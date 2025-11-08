f825f16f321a327de452a48e1728940f6c90b201
zfarmertic <lilyigbunu@gmail.com>
2025-11-08 15:25:29 +0100

    director created

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,8 @@
+var director1 = {
+    firstName: "ochuku",
+    lastName: "Uche",
+    fullTimeEmployee: false,
+    location: "oghara",
+    numberOfReports: 46
+};
+console.log(director1.lastName);
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,18 @@ interface Teacher {
     location: string;
     [key: string] : any
 }
+
+
+interface Director extends Teacher{
+    numberOfReports: number
+}
+
+const director1: Director = {
+    firstName: "ochuku",
+    lastName: "Uche",
+    fullTimeEmployee: false,
+    location: "oghara",
+    numberOfReports: 46
+}
+
+console.log(director1.lastName)
