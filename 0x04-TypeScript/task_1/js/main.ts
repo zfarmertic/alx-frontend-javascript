4e3c12ee53d9736abd46772e004f4622e6597752
zfarmertic <lilyigbunu@gmail.com>
2025-11-08 17:55:34 +0100

    task 5

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -46,3 +46,40 @@ class StudentClass implements StudentClassId{
         return this.firstName
     }
 }
+
+// Task 5 --- Advanced Types Part 1
+interface DirectorInterface{
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string
+}
+
+interface TeacherInterface{
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string
+}
+
+class Director implements DirectorInterface{
+     workFromHome(){
+        return " Working from home"
+     };
+    getCoffeeBreak(){
+        return "Getting a coffee break"
+    }
+    workDirectorTasks(){
+        return "Getting to director tasks"
+    }
+}
+
+class Teacher implements TeacherInterface{
+     workFromHome(){
+        return "Cannot work from home"
+     };
+    getCoffeeBreak(){
+        return "Cannot have a break"
+    }
+    workDirectorTasks(){
+        return "Getting to work"
+    }
+}
