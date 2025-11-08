1830d97cf56d65da9accce0abb610b813fbfbd41
zfarmertic <lilyigbunu@gmail.com>
2025-11-08 17:58:47 +0100

    task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,36 @@
+// Task 5 --- Advanced Types Part 1
+interface DirectorInterface{
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string
+}
+
+interface TeacherInterface{
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string
+}
+
+class Director implements DirectorInterface{
+     workFromHome(){
+        return " Working from home"
+     };
+    getCoffeeBreak(){
+        return "Getting a coffee break"
+    }
+    workDirectorTasks(){
+        return "Getting to director tasks"
+    }
+}
+
+class Teacher implements TeacherInterface{
+     workFromHome(){
+        return "Cannot work from home"
+     };
+    getCoffeeBreak(){
+        return "Cannot have a break"
+    }
+    workDirectorTasks(){
+        return "Getting to work"
+    }
+}
