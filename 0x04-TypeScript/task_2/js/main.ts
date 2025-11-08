73dad0f69b0f595ebb5c8b2d5b695ed9368d68b0
zfarmertic <lilyigbunu@gmail.com>
2025-11-08 18:50:30 +0100

    task 5

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,3 +34,40 @@ class Teacher implements TeacherInterface{
         return "Getting to work"
     }
 }
+
+//Task 6 --- Creating Function Specifi to employees
+
+interface Director {
+  workDirectorTasks(): string;
+}
+
+interface Teacher {
+  workTeacherTasks(): string;
+}
+
+function workDirectorTasks(): string {
+  return "Getting to director tasks";
+}
+
+function workTeacherTasks(): string {
+  return "Getting to work";
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Example usage
+const director: Director = { workDirectorTasks };
+const teacher: Teacher = { workTeacherTasks };
+
+console.log(executeWork(director)); // Getting to director tasks
+console.log(executeWork(teacher));  // Getting to work
