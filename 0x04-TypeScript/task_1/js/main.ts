10a6c52fa1aea35feb172cbeab59a0d5545229e0
zfarmertic <lilyigbunu@gmail.com>
2025-11-08 15:17:28 +0100

    interface teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,7 +2,7 @@ interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearsOfExperience: number;
+    yearsOfExperience?: number;
     location: string;
     [key: string] : any
 }
